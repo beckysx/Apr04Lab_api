f3ca3647e916f74bbf4c9f124b1403f812dc10b9
bexkysx <xu.song@centre.edu>
2019-04-04 23:28:45 -0400

    pictures

--- js.js
+++ js.js
@@ -2,30 +2,29 @@ var data=d3.json('https://ghibliapi.herokuapp.com/films')
 
 data.then(function(d){
 
-  var directors=[]
-  d.forEach(function(d){
-    if(directors.includes(d.director)==false){
-      directors.push(d.director)
-    }})
+      //get dataset
+      var directors=[]
+      d.forEach(function(d){
+        if(directors.includes(d.director)==false){
+          directors.push(d.director)
+        }})
+      var dataset=[]
+      directors.forEach(function(a,i){
+        dataset.push({"name":a,"films":[]})
+        d.forEach(function(d){
+          if (d.director==a){
+            var movie={"title":d.title,"release_date":d.release_date,"description":d.description,"picture":d.title+".png"}
+            dataset[i].films.push(movie)}
+        })})
 
-  var dataset=[]
-  directors.forEach(function(a,index){
-    dataset.push({"name":a,"films":[]})
-    d.forEach(function(d){
-      if (d.director==a){
-        var movie={"title":d.title,"release_date":d.release_date,"description":d.description}
-        dataset[index].films.push(movie)
-      }
-    })
-  })
-console.log(dataset)
-  d3.select("body").selectAll("button")
-  .data(dataset)
-  .enter()
-  .append("button")
-  .text(function(d){return d.name})
-  .on('click', function(d){
+      console.log(dataset)
+      d3.select("body").selectAll("button")
+      .data(dataset)
+      .enter()
+      .append("button")
+      .text(function(d){return d.name})
+      .on('click', function(d){
 
-  })
+      })
 
 },function(err){console.log(err)})
